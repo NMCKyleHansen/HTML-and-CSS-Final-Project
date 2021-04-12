f1ea5f4efb3aa7bc4a3903c6ee2fbed0020dfa18
NMCKyleHansen <72169091+NMCKyleHansen@users.noreply.github.com>
2021-04-12 11:03:07 -0400

    Add files via upload
    Initial version

---
+++ finalproject/index.js
@@ -0,0 +1,80 @@
+        function kali() {
+            var x = document.getElementById("KALI");
+            if (x.innerHTML === "Discover more about Kali Linux") {
+                x.innerHTML = "Kali Linux is an open-source, Linux distribution aimed at information security. It can be used for penetration testing, security research, ethical hacking, computer forensics and reverse engineering.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about Kali Linux";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+function puppy() {
+            var x = document.getElementById("PUPPY");
+            if (x.innerHTML === "Discover more about Puppy Linux") {
+                x.innerHTML = "Puppy Linux is a minimal footprint Linux distribution that runs well on legacy hardware such as old laptops that no longer function optimally with the current windows OS. Puppy Linux is also simple and easy to use.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about Puppy Linux";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+function suse() {
+            var x = document.getElementById("SUSE");
+            if (x.innerHTML === "Discover more about OpenSUSE") {
+                x.innerHTML = "OpenSUSE is a robust and popular Linux distribution. Many users are attracted to it because of the Tumbleweed package updating software that keep the OS and tools up to date.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about OpenSUSE";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+function fedora() {
+            var x = document.getElementById("FEDORA");
+            if (x.innerHTML === "Discover more about Fedora") {
+                x.innerHTML = "Fedora Linux is the free and open-source spin off Red Hat Linux and is sponsored by the Red Hat corporation. The benefits to using it are stability and an active community.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about Fedora";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+function ubuntu() {
+            var x = document.getElementById("UBUNTU");
+            if (x.innerHTML === "Discover more about Ubuntu") {
+                x.innerHTML = "Ubuntu Linux has consistently been one of the most popular Linux distributions for years. It is considered a solid Windows alternative and many computer hardware manufacturers will preinstall it on laptops and desktops. If you want safety in numbers, this a great distribution to use.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about Ubuntu";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+function popos() {
+            var x = document.getElementById("POPOS");
+            if (x.innerHTML === "Discover more about Pop!_OS") {
+                x.innerHTML = "Pop_OS! Is a newer Linux distribution that is becoming popular in the gaming community. The distribution comes pre-installed with Nvidia and AMD graphic card drivers.";
+		x.style.backgroundColor = "#FFFFFF";
+		x.style.color = "black";
+
+            } else {
+                x.innerHTML = "Discover more about Pop!_OS";
+		x.style.backgroundColor = "#424242";
+		x.style.color = "white";
+            }
+        }
+
+
